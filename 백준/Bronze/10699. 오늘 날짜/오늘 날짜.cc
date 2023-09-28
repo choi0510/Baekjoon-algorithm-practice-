4d236d7b188f588a2b5c32ed884955cc34c8f555
choi0510 <yh733609@gmail.com>
2023-09-28 17:04:27 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+
+int main() {
+	time_t timer = time(NULL);
+	struct tm* t = localtime(&timer);
+	cout << to_string(t->tm_year + 1900) << "-"; 
+	
+	cout.width(2);
+	cout.fill('0'); 
+	cout << to_string(t->tm_mon + 1) << "-";
+	
+	cout.width(2);
+	cout.fill('0');
+	cout << to_string(t->tm_mday);
+
+	return 0;
+}
