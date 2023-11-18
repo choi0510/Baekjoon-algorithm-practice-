d14f8a14bf0f830766c07a96247735ed4f987f30
choi0510 <yh733609@gmail.com>
2023-11-18 11:16:14 +0900

    [Silver IV] Title: 문자열 집합, Time: 68 ms, Memory: 7772 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, M;
+	int count = 0;
+	string str;
+	unordered_set<string> s;
+	
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+		s.insert(str);
+	}
+
+	for (int i = 0; i < M; i++) {
+		cin >> str;
+		if (s.find(str) != s.end()) {
+			count++;
+		}
+	}
+
+	cout << count;
+}
