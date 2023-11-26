a79fd6b962f61f2750ce44cd29aa51b1fb4cb463
choi0510 <yh733609@gmail.com>
2023-11-27 01:03:29 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <unordered_set>
+#include <algorithm>
+#include <vector>
 using namespace std;
+
+bool BS(vector<int>& v, int len, int target) {
+	int low = 0;
+	int high = len - 1;
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		if (v[mid] == target) {
+			return true;
+		}
+		else if (v[mid] < target) {
+			low = mid + 1;
+		}
+		else if (v[mid] > target) {
+			high = mid - 1;
+		}
+	}
+	return false;
+}
 
 int main() {
 
@@ -10,9 +30,12 @@ int main() {
 	int N, M;
 	int num;
 	unordered_set<int> s;
+	vector<int> v;
 
 	cin >> N;
-	for (int i = 0; i < N; i++) {
+
+	// undordered_set을 이용한 풀이
+	/*for (int i = 0; i < N; i++) {
 		cin >> num;
 		s.insert(num);
 	}
@@ -21,6 +44,26 @@ int main() {
 	for (int i = 0; i < M; i++) {
 		cin >> num;
 		if (s.find(num) != s.end()) {
+			cout << 1 << '\n';
+		}
+		else {
+			cout << 0 << '\n';
+		}
+	}*/
+
+	// binary search를 이용한 풀이
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		cin >> num;
+		if (BS(v, v.size(), num)) {
 			cout << 1 << '\n';
 		}
 		else {
