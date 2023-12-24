21da6959872fa1581df55c3187216854afffcd6a
choi0510 <yh733609@gmail.com>
2023-12-24 14:47:54 +0900

    [Bronze II] Title: 숫자, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	unsigned long long n1, n2;
+
+	cin >> n1 >> n2;
+	int min_n = min(n1, n2);
+	int max_n = max(n1, n2);
+	if (max_n - min_n < 2) {
+		cout << 0;
+	}
+	else {
+		cout << max_n - min_n - 1 << '\n';
+	}
+	for (unsigned long long i = min_n + 1; i < max_n; i++) {
+		cout << i << ' ';
+	}
+}
