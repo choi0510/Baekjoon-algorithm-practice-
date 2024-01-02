c0f13d2bb0de9a5106cabe7861b3c3bfdd52219f
choi0510 <yh733609@gmail.com>
2024-01-03 03:41:25 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, K;
+	int A, B;
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		cin >> A >> B;
+	}
+
+	cout << "비와이";
+}
