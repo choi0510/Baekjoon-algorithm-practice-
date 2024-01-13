f1bb1fab4b6be38179cbf872c285e20eb43dc6e9
choi0510 <yh733609@gmail.com>
2024-01-14 01:41:18 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	string str;
+	while (getline(cin, str)) {
+		cout << str << '\n';
+	}	
+}
