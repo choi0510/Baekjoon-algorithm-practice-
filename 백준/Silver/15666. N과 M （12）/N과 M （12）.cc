880b493219881df5f83adb58e35131a11b042d23
choi0510 <yh733609@gmail.com>
2024-01-23 15:26:13 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int real_arr[10];
+int arr[10];
+
+void backtracking(int k) {
+	if (k == M) {
+		for (int i = 0; i < M; i++) {
+			cout << real_arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	int check = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (check != arr[i]) {
+			if (k == 0) {
+				real_arr[k] = arr[i];
+				check = real_arr[k];
+				backtracking(k + 1);
+			}
+			else {
+				if (real_arr[k - 1] > arr[i]) {
+					continue;
+				}
+				real_arr[k] = arr[i];
+				check = real_arr[k];
+				backtracking(k + 1);
+			}
+		}
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int num;
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		arr[i] = num;
+	}
+
+	sort(arr, arr + N);
+
+	backtracking(0);
+}
