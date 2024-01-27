e57e082ca5330132477e26721f897b6334b740be
choi0510 <yh733609@gmail.com>
2024-01-28 01:50:02 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = i; j > 0; j--) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = i; j > 0; j--) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+}
