78b9e34ff6c48fbd19252780d13fdcfbd7b7d36f
choi0510 <yh733609@gmail.com>
2024-01-31 23:52:25 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 272 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -1,44 +1,45 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int arr[2000003];
-int tmp[2000003];
-
-void merge(int st, int en) {
-	int mid = (st + en) / 2;
-	int lidx = st;
-	int ridx = mid;
-
-	for (int i = st; i < en; i++) {
-		if (ridx == en) {
-			tmp[i] = arr[lidx++];
-		}
-		else if (lidx == mid) {
-			tmp[i] = arr[ridx++];
-		}
-		else if (arr[lidx] <= arr[ridx]) {
-			tmp[i] = arr[lidx++];
-		}
-		else {
-			tmp[i] = arr[ridx++];
-		}
-	}
-	
-	for (int i = st; i < en; i++) {
-		arr[i] = tmp[i];
-	}
-}
-
-void merge_sort(int st, int en) {
-	if (en - st == 1) {
-		return;
-	}
-	int mid = (st + en) / 2;
-
-	merge_sort(st, mid);
-	merge_sort(mid, en);
-	merge(st, en);
-}
+//int tmp[2000003];
+//
+//void merge(int st, int en) {
+//	int mid = (st + en) / 2;
+//	int lidx = st;
+//	int ridx = mid;
+//
+//	for (int i = st; i < en; i++) {
+//		if (ridx == en) {
+//			tmp[i] = arr[lidx++];
+//		}
+//		else if (lidx == mid) {
+//			tmp[i] = arr[ridx++];
+//		}
+//		else if (arr[lidx] <= arr[ridx]) {
+//			tmp[i] = arr[lidx++];
+//		}
+//		else {
+//			tmp[i] = arr[ridx++];
+//		}
+//	}
+//	
+//	for (int i = st; i < en; i++) {
+//		arr[i] = tmp[i];
+//	}
+//}
+//
+//void merge_sort(int st, int en) {
+//	if (en - st == 1) {
+//		return;
+//	}
+//	int mid = (st + en) / 2;
+//
+//	merge_sort(st, mid);
+//	merge_sort(mid, en);
+//	merge(st, en);
+//}
 
 int main() {
 
@@ -53,9 +54,15 @@ int main() {
 		cin >> arr[i];
 	}
 
-	merge_sort(0, N);
+	/*merge_sort(0, N);*/
 
-	for (int i = N - 1; i >= 0; i--) {
+	sort(arr, arr + N, greater<int>());
+
+	/*for (int i = N - 1; i >= 0; i--) {
+		cout << arr[i] << '\n';
+	}*/
+
+	for (int i = 0; i < N; i++) {
 		cout << arr[i] << '\n';
 	}
 }
