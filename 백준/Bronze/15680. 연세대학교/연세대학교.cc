5f3cd0374421c45518dc60662172f9a25471a4ef
choi0510 <yh733609@gmail.com>
2024-02-22 03:12:27 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+
+	cin >> N;
+
+	if (!N) {
+		cout << "YONSEI";
+	}
+	else {
+		cout << "Leading the Way to the Future";
+	}
+}
