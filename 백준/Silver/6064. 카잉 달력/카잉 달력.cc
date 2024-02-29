2e45e693d9c87677f3dc15568bcba98327253be8
choi0510 <yh733609@gmail.com>
2024-02-29 23:48:25 +0900

    [Silver I] Title: 카잉 달력, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int T;
+int M, N, x, y;
+bool isR;
+
+int gcd(int a, int b) {
+	if (a == 0) return b;
+	return gcd(b % a, a);
+}
+
+int lcm(int a, int b) {
+	return (a / gcd(a, b)) * b;
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> T;
+
+	while (T--) {
+		isR = false;
+		cin >> M >> N >> x >> y;
+		int LCM = lcm(M, N);
+		for (int i = x; i <= LCM; i += M) {
+			int key = i % N;
+			if (key == 0) {
+				key = N;
+			}
+			if (key == y) {
+				cout << i << '\n';
+				isR = true;
+				break;
+			}
+		}
+		if (!isR) {
+			cout << -1 << '\n';
+		}
+	}
+}
